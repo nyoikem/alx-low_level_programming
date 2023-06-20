cc06505ba6d9378c7de85aa8e21542cfea478ab5
nyoikem <nyoikem@gmail.com>
2023-06-20 09:55:16 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Check for alphabetic character
- * @c: The character to be checked
+ * @m: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
