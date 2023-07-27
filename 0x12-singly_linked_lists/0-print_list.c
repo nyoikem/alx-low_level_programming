66148be06b2a46c1cd9bf3a72fdeb7d8bd968375
nyoikem <wnyoike@yahoo.com>
2023-07-27 09:29:16 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,16 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
+
 		h = h->next;
 	}
 
