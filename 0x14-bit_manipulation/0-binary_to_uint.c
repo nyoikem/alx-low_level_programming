ce89e2d2cec32f1056d3d2beaa509fb8cecf2f1a
nyoikem <wnyoike@yahoo.com>
2023-09-28 07:22:16 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	if (len == 1 && b[0] == '0' || b[0] == '1')
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
 		return (b[0] - 48);
 	for (i = 0; b[i] != '\0'; i++)
 	{
