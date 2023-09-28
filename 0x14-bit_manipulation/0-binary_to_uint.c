fd4ad86cf63d2e1ea9982329291754ecf60d5040
nyoikem <wnyoike@yahoo.com>
2023-09-28 07:15:26 -0700

    crackme3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int len, i, j, sum, pow;
+	int base;
 
-	if (!b)
+	base = 2;
+	sum = 0;
+	pow = 1;
+
+	if (b == NULL)
 		return (0);
-
-	for (i = 0; b[i]; i++)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && b[0] == '0' || b[0] == '1')
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		for (j = len - 1; j > 0; j--)
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
 	}
-	return (dec_val);
+	return (sum);
 }
